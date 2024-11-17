b5579f39910a9b8480d42762ff057a09f0973a7b
lubunet <133784658+lubunet@users.noreply.github.com>
2024-11-17 14:17:03 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function comprarProduto(produto) {
-    alert(`Você escolheu comprar: ${produto}. Redirecionando para a página de pagamento...`);
-    // Aqui, você pode adicionar o redirecionamento para a página de pagamento.
-}
-
-function testeGratis(produto) {
-    alert(`Iniciando teste grátis para: ${produto}. Aproveite a versão de avaliação!`);
-    // Aqui, você pode adicionar lógica para iniciar o teste grátis.
-}
